ea140593d578dd087ef5fdb782c32e43ddda1906
Tzu-Han <48499652+Tzu-Han@users.noreply.github.com>
2021-10-14 09:13:40 +0800

    Add files via upload

---
+++ vega_lie_vis.js
@@ -0,0 +1,15 @@
+var vg_1 = "top_200_uni_map.json";
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+}).catch(console.error);
+var vg_2 = "size_year_score.json";
+vegaEmbed("#size_age_bubble_plot", vg_2).then(function(result) {
+}).catch(console.error);
+var vg_3 = "GDPvsTOP200_bubble.json";
+vegaEmbed("#GDP_bubble_plot", vg_3).then(function(result) {
+}).catch(console.error);
+var vg_4 = "top_1_uni_each_country.json";
+vegaEmbed("#stacked_bar", vg_4).then(function(result) {
+}).catch(console.error);
+var vg_4 = "2017-2021_top_10.json";
+vegaEmbed("#rank_line", vg_4).then(function(result) {
+}).catch(console.error);
